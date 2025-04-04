44206415b82f17444542df49157ccb3005d189bf
RonyRangelSS <rony.rangel@upe.br>
2025-04-04 20:01:56 -0300

    Adding animation into some widgets

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -5,24 +5,57 @@ import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 import 'package:meals_app/models/category.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   final List<Meal> availableMeals;
 
   const CategoriesScreen({required this.availableMeals, super.key});
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      lowerBound: 0,
+      upperBound: 1,
+      duration: const Duration(milliseconds: 300),
+    );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   void _selectCategory(BuildContext context, Category category) {
-    final filteredList = availableMeals.where((meal) => meal.categories.contains(category.id)).toList();
+    final filteredList =
+        widget.availableMeals
+            .where((meal) => meal.categories.contains(category.id))
+            .toList();
 
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (context) => MealsScreen(meals: filteredList, title: category.title,),
+        builder:
+            (context) =>
+                MealsScreen(meals: filteredList, title: category.title),
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: GridView(
         padding: EdgeInsets.all(24),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
@@ -39,7 +72,18 @@ class CategoriesScreen extends StatelessWidget {
                       _selectCategory(context, category);
                     },
                   ),
-                ).toList(),
+                )
+                .toList(),
+      ),
+      builder:
+          (context, child) => SlideTransition(
+            position:
+              Tween(
+                begin: Offset(0, 0.3), 
+                end: Offset(0, 0),
+                ).animate(CurvedAnimation(parent: _animationController, curve: Curves.easeInOut)),
+            child: child,
+          ),
     );
   }
 }
--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -22,20 +22,37 @@ class MealDetailsScreen extends ConsumerWidget {
                   .read(favoriteMealsProvider.notifier)
                   .toggleFavoritesMealsStatus(meal);
               ScaffoldMessenger.of(context).clearSnackBars();
-              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(wasAdded ? "Meal added as a favorite." : "Favorite meal removed."))); 
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    wasAdded
+                        ? "Meal added as a favorite."
+                        : "Favorite meal removed.",
+                  ),
+                ),
+              );
             },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(turns: Tween(begin: 0.7, end: 1.0).animate(animation), child: child,);
+              },
+              child: Icon(isFavorite ? Icons.star : Icons.star_border, key: ValueKey(isFavorite),),
+            ),
           ),
         ],
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Image.network(
-              meal.imageUrl,
-              height: 300,
-              width: double.infinity,
-              fit: BoxFit.cover,
+            Hero(
+              tag: meal.id,
+              child: Image.network(
+                meal.imageUrl,
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
             ),
             SizedBox(height: 14),
             Text(
